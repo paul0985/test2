ed41d79e0ba751713835b339c2ca3b77fe266fa3
paul0985 <ps5294773@gmail.com>
2026-04-04 19:47:07 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {	
+
+	// Scroll to Top
+	jQuery('.scrolltotop').click(function(){
+		jQuery('html').animate({'scrollTop' : '0px'}, 550);
+		return false;
+	});
+	
+	jQuery(window).scroll(function(){
+		var upto = jQuery(window).scrollTop();
+		if(upto > 500) {
+			jQuery('.scrolltotop').fadeIn();
+		} else {
+			jQuery('.scrolltotop').fadeOut();
+		}
+	});
+
+
+	
+		
+});
